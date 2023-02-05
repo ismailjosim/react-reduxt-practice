472f7c7c32c813887e8cbf31e74c41c5ab4ff5b9
Md. Jasim <75038630+ismailjosim@users.noreply.github.com>
2023-02-05 21:09:56 +0600

    add file from react-redux old way

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Provider } from 'react-redux';
+import { RouterProvider } from "react-router-dom";
+import store from './redux/store/store';
+import routes from "./routes/routes";
+
+function App() {
+  return (
+    <Provider store={ store }>
+      <RouterProvider router={ routes } />
+    </Provider>
+  );
+}
+
+export default App;
