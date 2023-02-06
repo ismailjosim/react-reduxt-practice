ac9202a091adcc2a72920aecc63e37346d7322b6
Md. Jasim <75038630+ismailjosim@users.noreply.github.com>
2023-02-07 00:50:24 +0600

    add proudct quantity

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -13,12 +13,19 @@ const ProductCard = ({ product }) => {
   const { pathname } = useLocation();
 
   return (
-    <div className='shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900'>
+    <div className='shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900 relative'>
       <div className='h-52 w-52 mx-auto'>
         <img src={ product.image } alt={ product.model } />
       </div>
       <h1 className='font-bold text-center'>{ product.model }</h1>
       <p className='text-center font-semibold mb-3'>Rating: { product.rating }</p>
+      <div className='absolute'>
+        { pathname.includes("cart") && (<button className='text-white text-bold px-5 bg-gray-400 py-1 rounded-full'
+        >Total: { product.quantity }
+        </button>
+        ) }
+      </div>
+
       <div className=' flex-1'>
         <ul className='space-y-2'>
           { product.keyFeature.map((feature, idx) => {
@@ -26,15 +33,17 @@ const ProductCard = ({ product }) => {
           }) }
         </ul>
       </div>
+
       <div className='flex gap-2 mt-5'>
         {
           pathname.includes("cart") && (
-            <button className='bg-red-500 rounded-full w-full flex justify-center items-center gap-1 py-2 px-2 text-white text-bold'
-              onClick={ () => dispatch(removeFromCart(product)) }
-            >
+            <button
+              className='bg-red-500 rounded-full w-full flex justify-center items-center gap-1 py-2 px-2 text-white text-bold'
+              onClick={ () => dispatch(removeFromCart(product)) }>
               <BiTrash />
               <span>Remove</span>
             </button>
+
           )
         }
         {
